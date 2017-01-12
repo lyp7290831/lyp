2abb8744070f6850cfb06a4a32425432931ee79c
lyp110 <281822323@qq.com>
2017-01-12 16:58:28 +0800

    框架

---
+++ BiLiBiLi/app/app.js
@@ -0,0 +1,8 @@
+angular.module("com.bili",
+    [
+        "ionic",
+        "com.bili.controllers",
+        "com.bili.directives",
+        "com.bili.services",
+        "com.bili.configs"
+    ]);
